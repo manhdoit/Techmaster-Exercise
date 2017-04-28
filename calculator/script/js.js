567737f00e58ebea8f698735c2d242d645edbbc2
manhdoit <nguyenmanhdo@techmasters-Mac-mini-6.local>
2017-04-28 15:45:32 +0700

    1

--- calculator/script/js.js
+++ calculator/script/js.js
@@ -1,72 +1,100 @@
 var output,
-	luu_kq,
-	operator,
-	screen;
+luu_kq,
+operator,
+lastChar,
+isResult = false,
+screen;
+
 window.onload = function(){
-
-	//function hienthi() {//Click số hiển thị lên màn hình
-		screen = document.getElementById("result");
-		var btn = document.querySelectorAll(".btn_number");
-		var len_btn = btn.length;
-		for(var i = 0; i < len_btn; i++) {
-			btn[i].addEventListener("click", function() {
-				num = this.innerHTML;
-				screen.innerHTML += num;
-				output = screen.innerHTML;
-			})
-		}
-	// }
-	// hienthi();
-
-	function operator() {//Click các button toán tử
-		operator = document.querySelectorAll(".operator");
-		var len_operator = operator.length;
-		for (var i = 0; i < len_operator; i++) {
-			operator[i].addEventListener("click", function(){
-				operator = this.innerHTML;
-				if (screen.innerHTML == "") {
-					screen.innerHTML = screen.innerHTML;
-				}
-				
-				else if(luu_kq = screen.innerHTML) {//Nếu thực hiện phép tính xong mà ấn tiếp toán tử
-					screen.innerHTML = luu_kq + operator;
-				}
-				else {
-					screen.innerHTML = output + operator;
+	screen = document.getElementById("result");
+	//Click số hiển thị lên màn hình
+	var btn = document.querySelectorAll(".btn_number");
+	var len_btn = btn.length;
+	for(var i = 0; i < len_btn; i++) {
+		btn[i].addEventListener("click", function() {
+			num = this.innerHTML;
+			output = screen.innerHTML;
+			if(isResult) {
+				output = screen.innerHTML = num;
+				isResult = false;
+			}	
+			else {
+				output = screen.innerHTML += num;
 			}
 		})
-			
+	}
+//Click các button toán tử
+operator = document.querySelectorAll(".operator");
+var len_operator = operator.length;
+for (var i = 0; i < len_operator; i++) {
+	operator[i].addEventListener("click", function(){
+		operator = this.innerHTML;
+		if (screen.innerHTML == "") {
+			screen.innerHTML = screen.innerHTML;
 		}
-	}
-	operator();
-
-	function giaithua() {
-		
-	}
-	function ketqua() {
-		document.querySelector(".btn_equals").addEventListener("click", function() {
-			screen.innerHTML = eval(output);
+		else if(luu_kq = screen.innerHTML) {//Nếu thực hiện phép tính xong mà ấn tiếp toán tử
+			screen.innerHTML = luu_kq + operator;
+			isResult = false;
+		}
+		else {
+				screen.innerHTML = output + operator;
+		}
 		})
-	}
-	ketqua();
+}
 
-	function AC() {//Xóa màn hình hiển thị
+Can_b2 = document.querySelector(".btn_sqrt");
+Can_b2.addEventListener("click", function(){
+	if (screen.innerHTML == output) {
+		screen.innerHTML = Math.sqrt(output);
+		isResult = true;
+	}
+})
+	//phần trăm
+	PhanTram = document.querySelector(".btn_percent");
+	PhanTram.addEventListener("click", function(){
+		if(screen.innerHTML == output) {
+			screen.innerHTML = eval(output/100);
+			isResult = true;
+		}
+	})
+	//bình phương
+	BinhPhuong = document.querySelector(".btn_square");
+	BinhPhuong.addEventListener("click", function(){
+		if (screen.innerHTML == output) {
+			screen.innerHTML = Math.pow(output,2);
+		}
+	})
+	//tính giai thừa
+	GiaiThua = document.querySelector(".btn_factorial");
+	GiaiThua.addEventListener("click", function() {
+		if (screen.innerHTML == output) {
+			screen.innerHTML = giaithua(output);
+			isResult = true;
+		}
+	})
+	//hàm tính giai thừa
+	function giaithua(n) {
+		var j=1;
+		for (var i = 2; i <= n; i++) {
+			j = j*i;
+		}
+		return j;
+	}
+	//Ân nút =
+	document.querySelector(".btn_equals").addEventListener("click", function() {
+		var KetQua = eval(output);
+		screen.innerHTML = KetQua;
+		isResult = true;
+	})
+		//ấn nút AC
 		var xoa = document.querySelector(".btn_clear");
 		xoa.addEventListener("click", function(){
 			screen.innerHTML = "";
+			output = "";
 		})
-	}
-	AC();
-	
-	function Del() {//ấn nút mũi tên xóa ký tự
-		var del = document.querySelector(".btn_del");
-		del.addEventListener("click", function() {
-			screen.innerHTML = screen.innerHTML.slice(0,-1);
-		})
-	}
-	Del();
-	
-	
-	
-
+	//ấn nút mũi tên xóa ký tự
+	var del = document.querySelector(".btn_del");
+	del.addEventListener("click", function() {
+		screen.innerHTML = screen.innerHTML.slice(0,-1);
+	})
 }

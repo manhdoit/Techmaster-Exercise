03475d5d5e8964a3ed95ae34271d16a38b43629a
nguyenmanhdo <manhdo.it@gmail.com>
2017-06-02 10:06:56 +0700

    abc

---
+++ image gallery/js.js
@@ -0,0 +1,10 @@
+/* Created by nguyenmanhdo on 6/1/17.
+*/
+$(document).ready(function(){
+  $("header, section ").addClass("wrapper");
+  $(".row_img > div").addClass("col");
+  $(".col").append($('<div class="height-light"></div>'));
+  $(".height-light").append($('<h1>Lorem ipsum dolor sit amet</h1> <p>Kiểu tóc uốn cúp đuôi này cũng là kiểu tóc khiến các bạn nữ Việt Nam bị hút hồn</p>'))
+
+})
+

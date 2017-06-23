ca05d38095b95b2bfaebaf6e7c0d22456e0a7f85
nguyenmanhdo <manhdo.it@gmail.com>
2017-06-23 17:51:30 +0700

    add

--- Project-Bán xe/script/js.js
+++ Project-Bán xe/script/js.js
@@ -22,30 +22,50 @@ $('#logout-btn').click(function(){
     event.preventDefault();
 });
 // -----------------------Banner Carousel------------------------------
-var $item = $('.carousel .item');
-var $wHeight = $(window).height();
-$item.eq(0).addClass('active');
-// $item.height($wHeight);
-$item.addClass('full-screen');
-
-$('.carousel img').each(function() {
-    var $src = $(this).attr('src');
-    var $color = $(this).attr('data-color');
-    $(this).parent().css({
-        'background-image' : 'url(' + $src + ')',
-        'background-color' : $color
-    });
-    $(this).remove();
+// var $item = $('.carousel .item');
+// var $wHeight = $(window).height();
+// $item.eq(0).addClass('active');
+// // $item.height($wHeight);
+// $item.addClass('full-screen');
+//
+// $('.carousel img').each(function() {
+//     var $src = $(this).attr('src');
+//     var $color = $(this).attr('data-color');
+//     $(this).parent().css({
+//         'background-image' : 'url(' + $src + ')',
+//         'background-color' : $color
+//     });
+//     $(this).remove();
+// });
+//
+// $(window).on('resize', function (){
+//     $wHeight = $(window).height();
+//     $item.height($wHeight);
+// });
+//
+// $('.carousel').carousel({
+//     interval: 6000,
+//     pause: "false"
+// });
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+    interval: false
 });
 
-$(window).on('resize', function (){
-    $wHeight = $(window).height();
-    $item.height($wHeight);
-});
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
 
-$('.carousel').carousel({
-    interval: 6000,
-    pause: "false"
+    if (next.next().length>0) {
+        next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
 });
 // ---------------------------Google Map API-------------------------
 
@@ -117,3 +137,5 @@ $('.edit').on("click", function(){
     // $(this).closest('input').removeAttr('disabled');
 })
 // ----------------------
+
+// ===============Click comment in blog================

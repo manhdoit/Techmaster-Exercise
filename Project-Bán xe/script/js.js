7fe177ebdc79c5229bd6afa1166168e509afae11
manhdoit <manhdo.it@gmail.com>
2017-06-28 00:24:37 +0700

    egdfgdfg

--- Project-Bán xe/script/js.js
+++ Project-Bán xe/script/js.js
@@ -139,3 +139,13 @@ $('.edit').on("click", function(){
 // ----------------------
 
 // ===============Click comment in blog================
+var angle = 0;
+function gallerySpin(sign) {
+    var spinner = $("#spinner");
+    if (!sign) {
+        angle = angle + 45;
+    } else {
+        angle = angle - 45;
+    }
+    spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+}
